bfbea256528089d0eab1c205aa6dc06c9b340d7c
Your Name <your-email@example.com>
2025-07-14 00:08:13 +0300

    Deploy: 2025-07-14_00-08-12

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import FavoritesView from '../views/FavoritesView.vue'
 import OrdersView from '../views/OrdersView.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
